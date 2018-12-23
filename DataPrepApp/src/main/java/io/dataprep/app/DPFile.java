b9a9a3a7c297c798207a4032a81e7e5d84d678dc
scook <scook@nmetric.com>
2018-12-22 23:00:55 -0700

    numlines

--- DataPrepApp/src/main/java/io/dataprep/app/DPFile.java
+++ DataPrepApp/src/main/java/io/dataprep/app/DPFile.java
@@ -1,7 +1,11 @@
 package io.dataprep.app;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 
 import com.opencsv.CSVReader;
 
@@ -13,12 +17,14 @@ import lombok.Data;
  * @author Stephen Cook
  *
  */
+
 @Data
 public class DPFile {
 
 	private String fileName;
 	private String fullFileName; // includes full path
 	private String fileSize;
+	private long numLines;
 	
 	private String[] headers;
 	private String[] colTypes;
@@ -28,12 +34,18 @@ public class DPFile {
 	}
 	
 	public void readFullFile() throws IOException {
+		@SuppressWarnings("resource")
 		CSVReader reader = new CSVReader(new FileReader(fullFileName));
 	    String [] nextLine;
 	    
 	    headers = reader.readNext();
+	    if(headers==null || headers.length<1) {
+	    	System.out.println("Fatal error.  Header line is required but is not available.");
+			return;
+	    }
 	    parseHeadline();
-	    
+	    numLines = countLines();
+	    System.out.println("Total Lines = "+numLines);
 	    while ((nextLine = reader.readNext()) != null) {
 	        // nextLine[] is an array of values from the line
 	    	String fullLine = String.join(", ", nextLine);
@@ -47,11 +59,46 @@ public class DPFile {
 	}
 
 	private void parseHeadline() {
-		if(headers.length<1) {
-			System.out.println("Fatal error.  Header line is required but is not available.");
-			return;
-		}
+		System.out.println(String.join(" , ", headers));
 		
 	}
 	
+	private long countLines() throws IOException {
+	    InputStream is = new BufferedInputStream(new FileInputStream(fullFileName));
+	    try {
+	        byte[] c = new byte[1024];
+
+	        int readChars = is.read(c);
+	        if (readChars == -1) {
+	            // bail out if nothing to read
+	            return 0;
+	        }
+
+	        // make it easy for the optimizer to tune this loop
+	        long count = 0;
+	        while (readChars == 1024) {
+	            for (int i=0; i<1024;) {
+	                if (c[i++] == '\n') {
+	                    ++count;
+	                }
+	            }
+	            readChars = is.read(c);
+	        }
+
+	        // count remaining characters
+	        while (readChars != -1) {
+	            System.out.println(readChars);
+	            for (int i=0; i<readChars; ++i) {
+	                if (c[i] == '\n') {
+	                    ++count;
+	                }
+	            }
+	            readChars = is.read(c);
+	        }
+
+	        return count == 0 ? 1 : count;
+	    } finally {
+	        is.close();
+	    }
+	}
 }
